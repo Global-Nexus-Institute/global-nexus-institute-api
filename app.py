c39ef5447c17bfe02937e1aeb114aea5bdddb871
Elcid-Chawa <elcidchawa@gmail.com>
2024-10-12 21:14:41 +0200

    add mongo db to configurations

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app as application
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5003, host="0.0.0.0")
+    application.run(debug=True, port=5003, host="0.0.0.0")
